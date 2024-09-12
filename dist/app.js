a00df91b3088ebb68aeeced7d97a67938275be6d
Omkar Sathish <omkarsathish06@gmail.com>
2024-09-12 23:20:35 +0530

    add functionalities

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import { nanoid } from "nanoid";
 import connectDB from "./utils/connectDB.js";
+import ShortURL from "./models/user.js";
+import ErrorHandler from "./utils/errorHandler.js";
 dotenv.config({ path: "./.env" });
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const port = process.env.PORT || 3000;
@@ -15,9 +18,62 @@ app.use(cors({ origin: " * ", credentials: true }));
 app.use(morgan("dev"));
 app.get("/api", (req, res) => {
     const _html = `
-    <h1 style="width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center;">CI/CD Pipeline for NodeJS Apps</h1>
+    <h1 style="font-family: Sans-Serif; width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center;">CI/CD Pipeline Project for NodeJS Apps</h1>
   `;
     res.send(_html);
+});
+app
+    .route("/api/short-url/:url")
+    .post(async (req, res) => {
+    try {
+        const { url } = req.params;
+        const smallURL = nanoid(10);
+        const ans = new ShortURL({
+            url,
+            shortUrl: smallURL,
+            clickCount: 0,
+        });
+        await ans.save();
+        res.send(ans.shortUrl);
+    }
+    catch (error) {
+        throw new ErrorHandler(500, "Internal Server Error");
+    }
+})
+    .get(async (req, res) => {
+    try {
+        const { url } = req.params;
+        const ans = await ShortURL.findOne({
+            shortUrl: url,
+        });
+        if (!ans) {
+            return res.status(400).send({ error: "Invalid Url" });
+        }
+        ans.clickCount++;
+        await ans.save();
+        res.status(200).send(`${ans.url}`);
+    }
+    catch (error) {
+        throw new ErrorHandler(500, "Internal Server Error");
+    }
+});
+app.get("/api/stats/:url", async (req, res) => {
+    try {
+        const { url } = req.params;
+        const ans = await ShortURL.findOne({
+            shortUrl: url,
+        });
+        if (!ans) {
+            throw new ErrorHandler(400, "Invalid Url");
+        }
+        const _html = `
+      <h1 style="font-family: Sans-Serif; width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center;">Click Count: ${ans.clickCount}</h1>
+    `;
+        res.send(_html);
+    }
+    catch (error) {
+        throw new ErrorHandler(500, "Internal Server Error");
+    }
 });
 app.get("*", (req, res) => {
     res.status(404).json({
---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const schemaDefinition = {
+  url: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  clickCount: {
+    type: Number,
+    required: true,
+  },
+};
+
+const options = { timestamps: true };
+
+const urlModel = new mongoose.Schema(schemaDefinition, options);
+
+const ShortURL = mongoose.model("ShortURL", urlModel);
+
+export default ShortURL;
